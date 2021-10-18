72d0e021f2ff0104b5993e772b7bf70daffc70f2
giovanicaf <giovanicaf@gmail.com>
2021-10-17 21:53:21 -0300

    Category implementation in memory

---
+++ src/main/java/com/github/giovanicaf/productrepository/entities/Category.java
@@ -0,0 +1,39 @@
+package com.github.giovanicaf.productrepository.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/github/giovanicaf/productrepository/resource/CategoryResource.java
@@ -0,0 +1,38 @@
+package com.github.giovanicaf.productrepository.resource;
+
+import com.github.giovanicaf.productrepository.entities.Category;
+import com.github.giovanicaf.productrepository.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+    @Autowired
+    private CategoryRepository category;
+
+
+    @PostMapping
+    public void saveCategory(@RequestBody  Category cat){
+        category.save( cat );
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll() {
+
+        List<Category> list = category.findAll() ;
+        return ResponseEntity.ok().body( list );
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Category> findById(@PathVariable Long id) {
+
+        Category cat = category.findById(id) ;
+        return ResponseEntity.ok( ).body( cat  );
+    }
+}
